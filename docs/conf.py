04abb14ddce88ff00582409d9a1f3ed220795118
Golam Shaifullah <golam.shaifullah@unimib.it>
2026-02-27 17:37:32 +0100

    Remove bug in docs building.

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+# src-layout project: add ../src so autodoc/autosummary can import pqc
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "pqc"
 extensions = [
